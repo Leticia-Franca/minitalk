9d016de94752648a0624994dc9ebe75bb60fab2d
Leticia <canumsang@gmail.com>
2021-11-17 03:07:35 -0300

    header for client

---
+++ client.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lfranca- <lfranca-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/11 22:31:41 by lfranca-          #+#    #+#             */
+/*   Updated: 2021/11/17 03:06:13 by lfranca-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
+#include <sys/types.h>
+#include "./libft/libft.h"
+
+void ft_error (char **to_free);
