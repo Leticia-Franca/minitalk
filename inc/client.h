0832f6b3a48f58e818d8ee13351a140286d21536
user42 <canumsang@gmail.com>
2021-11-30 17:50:05 -0300

    folder for client.h and server.h

---
+++ inc/client.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lfranca- <lfranca-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/11 22:31:41 by lfranca-          #+#    #+#             */
+/*   Updated: 2021/11/29 03:22:12 by lfranca-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLIENT_H
+# define CLIENT_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <signal.h>
+# include <string.h>
+# include <sys/types.h>
+# include "../libft/libft.h"
+
+void	ft_error(char **to_free);
+
+#endif
---
+++ inc/server.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lfranca- <lfranca-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/11 22:41:58 by lfranca-          #+#    #+#             */
+/*   Updated: 2021/11/29 03:21:24 by lfranca-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SERVER_H
+# define SERVER_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <signal.h>
+# include <string.h>
+# include <sys/types.h>
+# include "../libft/libft.h"
+
+void	hand_sigs(int signo, siginfo_t *info, void *context);
+void	ft_putchar_adapt(char a);
+
+#endif
