698d2bf165bc7462c83e6d678ad40ab6e329e36d
user42 <canumsang@gmail.com>
2021-11-30 17:47:48 -0300

    libft updated with the new norm

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -6,13 +6,13 @@
 /*   By: lfranca- <lfranca-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/20 22:15:29 by lfranca-          #+#    #+#             */
-/*   Updated: 2021/06/01 16:18:37 by lfranca-         ###   ########.fr       */
+/*   Updated: 2021/11/29 03:12:17 by lfranca-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memchr (const void *s, int c, size_t n)
+void	*ft_memchr(const void *s, int c, size_t n)
 {
 	size_t			i;
 	unsigned char	*pt_s;
